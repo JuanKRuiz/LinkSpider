1a401a3961f6a3b8988c7facad6f2b159302b889
JuanK <juank.ruiz@gmail.com>
2014-04-25 01:22:35 -0500

    1 prototype

---
+++ SiteMapperBash/Program.cs
@@ -0,0 +1,28 @@
+﻿using SiteMapperLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteMapperBash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkSpider ls = new LinkSpider();
+            var hc = new HttpClient();
+            var htmlFragment = hc.GetStringAsync("http://juank.io").Result;
+
+            var lista = ls.GetCompleteLinksFromHtmlFragment(htmlFragment, "http://juank.io");
+
+            foreach (var link in lista)
+            {
+                Console.WriteLine( link);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SiteMapperLib/LinkSpider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SiteMapperLib
+{
+    public class LinkSpider
+    {
+        readonly Regex rx = new Regex(@"<a.*?href=(""|')(?<href>.*?)(""|').*?>(?<value>.*?)</a>");
+  
+        public List<string> GetLinksFromHtmlFragment(string htmlFragment)
+        {
+            var lista = new List<string>();
+            foreach (Match match in rx.Matches(htmlFragment))
+            {
+                lista.Add(match.Groups["href"].Value);
+            }
+
+            return lista;
+        }
+
+        public List<string> GetCompleteLinksFromHtmlFragment(string htmlFragment, string baseUrl)
+        {
+            UriBuilder uribldr = new UriBuilder(baseUrl);
+
+            var lista = new List<string>();
+            foreach (Match match in rx.Matches(htmlFragment))
+            {
+                var link = match.Groups["href"].Value;
+                
+                if(link.StartsWith("/") || !link.Contains("://"))
+                {
+                    uribldr.Path = link;
+                    uribldr.Query = string.Empty;
+                    lista.Add(uribldr.Uri.AbsoluteUri);
+                }
+                else if (link.StartsWith(baseUrl))
+                {
+                    var uri = new Uri(link);
+                    uribldr.Path = uri.AbsolutePath;
+                    uribldr.Query = string.Empty;
+                    lista.Add(uribldr.Uri.AbsoluteUri);
+                }
+            }
+
+            return lista;
+        }
+
+    }
+}
