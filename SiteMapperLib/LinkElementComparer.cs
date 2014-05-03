381c68adb688326bce373bcf532267347100749c
JuanK <juank.ruiz@gmail.com>
2014-05-02 19:55:23 -0500

    .

---
+++ SiteMapperLib/LinkElementComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SiteMapperLib
+{
+                    
+    public class LinkElementComparer: IEqualityComparer<LinkElement>
+    {
+        public bool Equals(LinkElement x, LinkElement y)
+        {
+            return x.url == y.url;
+        }
+
+        public int GetHashCode(LinkElement obj)
+        {
+            return obj.url.GetHashCode();
+        }
+    }
+}
